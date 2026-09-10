060d9de966ef04cb2a5b53663b9fd71fb3e3a2da
bituanamony-dot <bituanamony@gmail.com>
2026-09-09 22:23:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import gspread
-from google.oauth2client.service_account import Credentials
+from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 
 st.set_page_config(page_title="BITU", page_icon="🛒", layout="wide")
 
 # --- CONEXION A GOOGLE SHEET ---
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
-creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
 client = gspread.authorize(creds)
 sheet = client.open_by_url(st.secrets["sheet_url"]).sheet1
 
